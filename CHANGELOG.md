1caf10a0fd1b93c8d56d76954fcccbc6fa523b9d
Andreas Christodolou <andreas@andach.co.uk>
2023-09-18 11:33:38 +0100

    Fix attachments and comments bug

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
 # Changelog
 
 All notable changes to `laravel-view-components` will be documented in this file.
+
+## v1.0.0
+Initial release. Includes various components and pagination. 
+
+## v1.0.1
+Fixed bug in attachments-and-comments which was still calling <x-card> instead of <x-andach-card>.
--- resources/views/general/attachments-and-comments.blade.php
+++ resources/views/general/attachments-and-comments.blade.php
@@ -1,12 +1,12 @@
 @if (count($model->attachmentsAndComments()))
-    <x-card title="Attachments and Comments" class="mt-4">
+    <x-andach-card title="Attachments and Comments" class="mt-4">
         @foreach ($model->attachmentsAndComments() as $item)
             {!! Blade::render($item['display_html']) !!}
         @endforeach
-    </x-card>
+    </x-andach-card>
 @endif
 
-<x-card title="Add Attachment and/or Comment" class="mt-4">
+<x-andach-card title="Add Attachment and/or Comment" class="mt-4">
     <x-form-textarea label="Add New Comment" name="new_comment"/>
     <x-form-input type="file" label="Add Attachment" name="new_attachment"/>
-</x-card>
+</x-andach-card>
